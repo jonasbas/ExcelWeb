677c373f13a78d9b866f821edbad0e1b2a79ad90
Jonas Bastisch <jonas.bastisch@gmail.com>
2021-11-02 12:41:00 +0100

    Update ExcelController.java

--- src/main/java/web/excel/ExcelController.java
+++ src/main/java/web/excel/ExcelController.java
@@ -1,12 +1,19 @@
 package web.excel;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
@@ -16,27 +23,37 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class ExcelController {
 	
+	//Get Request für die Startseite
 	@GetMapping("/")
 	public String home() {
+		//löschen Datei falls sie nicht gedownloadet wurde
+		File file = new File("ergebnisse.xlsx");
+		if(file.exists()) {
+			file.delete();
+		}
 		return "Main.html";
 	}
 	
+	//Post Request für den Berechnen Knopf wertet die Excel Datei aus und schreibt eine neue
 	@PostMapping("/upload")
 	public String upload(@RequestParam("uploadFile") MultipartFile file){
 		try {
+			//falls keine Datei übergeben wurde 
 			if(file.isEmpty()) {
 				return "NoFile.html";
 			}
 			else {
+				//übergen die Datei an eine Helferklasse die die Verwertung übernimmt
 				ExcelConverter conv = new ExcelConverter(file);
 				Workbook resultWb = conv.excel();
-				OutputStream fileOut = new FileOutputStream("tmp.xlsx");
+				OutputStream fileOut = new FileOutputStream("ergebnisse.xlsx");
 				resultWb.write(fileOut);
 				fileOut.close();
 			}
@@ -51,16 +68,38 @@ public class ExcelController {
 	}
 	
 	@GetMapping("/download")
-	public ResponseEntity<Resource> download() throws FileNotFoundException {
-	    String filename = "ergebnisse.xlsx";
+	public void dowloadAndDelete(HttpServletResponse response) throws IOException {
+		
+	    File file = new File("ergebnisse.xlsx");
 	    
-	    InputStream in = new FileInputStream("tmp.xlsx");
-	    InputStreamResource file = new InputStreamResource(in);
-	    
-	    return ResponseEntity.ok()
-	        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + filename)
-	        .contentType(MediaType.parseMediaType("application/vnd.ms-excel"))
-	        .body(file);
-	  }
+	    //Setzen Response Header für den File Download
+	    response.setContentType("application/vnd.ms-excel");
+	    response.setHeader("Content-disposition", "attachment; filename=" + file.getName());
+
+	    OutputStream out = response.getOutputStream();
+	    FileInputStream in = new FileInputStream(file);
+
+	    // in nach out kopieren 
+	    IOUtils.copy(in,out);
+
+	    //Schließen Input und Outputstream und löschen die Excel Datei von der Festplatte
+	    out.close();
+	    in.close();
+	    file.delete();
+	}
+
+//	Andere Lösung ohne Datei löschen
+//	@GetMapping("/download")
+//	public ResponseEntity<Resource> download() throws FileNotFoundException {
+//	    String filename = "ergebnisse.xlsx";
+//	    
+//	    InputStream in = new FileInputStream("tmp.xlsx");
+//	    InputStreamResource file = new InputStreamResource(in);
+//	   
+//	    return ResponseEntity.ok()
+//	        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + filename)
+//	        .contentType(MediaType.parseMediaType("application/vnd.ms-excel"))
+//	        .body(file);
+//	  }
 	
 }
